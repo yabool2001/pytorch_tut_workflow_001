4b5ac49fd31cfebc00807946886f3d460094f384
yabool2001 <mzemlo.pl@gmail.com>
2023-06-08 10:36:33 +0200

    aaaa

--- pytorch_tut_vision_v2.py
+++ pytorch_tut_vision_v2.py
@@ -1,3 +1,4 @@
+# Rozgrzebane
 # 2023.06.08
 # Source: https://www.learnpytorch.io/03_pytorch_computer_vision/
 # Computer vision is the art of teaching a computer to see.
@@ -65,30 +66,53 @@ def train_model ( model , X_train , loss_fn ) :
         if epoch % ( round ( train_epochs / 10 ) ) == 0 :
             print ( f"Epoch: {epoch} | Loss: {loss:.5f} | Accuracy: {acc:.2f}%" )
 
-class CircleModelV3 ( nn.Module ) :
-# Construct a model (oryginal V2) class that subclasses nn.Module with non-linear activation function
-# Jest lepsze niż nn.Sequential w przypadkach bardziej skomplikowanych struktur
-# Oryginal version comprises 3 hidden layers and 10 features
-    def __init__ ( self ):
-        super ().__init__ ()
-        # Create 3 nn.Linear layers capable of handling X and y input and output shapes
-        self.layer_1 = nn.Linear(in_features=2, out_features=10)
-        self.layer_2 = nn.Linear(in_features=10, out_features=10)
-        self.layer_3 = nn.Linear(in_features=10, out_features=1)
-        # Create 2 nn.Linear layers capable of handling X and y input and output shapes
-        # self.layer_1 = nn.Linear ( in_features =  2 , out_features = 3 ) # takes in 2 features (X), produces 3 features
-        # self.layer_2 = nn.Linear ( in_features = 3 , out_features = 1 ) # takes in 3 features (X), produces 10 features
-        # self.layer_3 = nn.Linear ( in_features = 10 , out_features =  1 ) # takes in 10 features, produces 1 feature (y)
-        self.relu = nn.ReLU () # <- add in ReLU activation function
-        # Can also put sigmoid in the model 
-        # This would mean you don't need to use it on the predictions
-        # self.sigmoid = nn.Sigmoid()
+# Create a convolutional neural network 
+class FashionMNISTModelV2(nn.Module):
+    """
+    Model architecture copying TinyVGG from: 
+    https://poloclub.github.io/cnn-explainer/
+    """
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                      out_channels=hidden_units, 
+                      kernel_size=3, # how big is the square that's going over the image?
+                      stride=1, # default
+                      padding=1),# options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2) # default stride value is same as kernel_size
+        )
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            # Where did this in_features shape come from? 
+            # It's because each layer of our network compresses and changes the shape of our inputs data.
+            nn.Linear(in_features=hidden_units*7*7, 
+                      out_features=output_shape)
+        )
     
-    # 3. Define a forward method containing the forward pass computation
-    def forward ( self , x ) :
-        # Intersperse the ReLU activation function between layers
-        return self.layer_3 ( self.relu ( self.layer_2 ( self.relu ( self.layer_1 ( x ) ) ) ) )
-        # return self.layer_2 ( self.relu ( self.layer_1 ( x ) ) )
+    def forward(self, x: torch.Tensor):
+        x = self.block_1(x)
+        # print(x.shape)
+        x = self.block_2(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        # print(x.shape)
+        return x
 
 # #############
 # ### START APP
@@ -122,6 +146,11 @@ print ( f"\n{image.shape = }")
 print ( f"\n{label = }" )
 print ( f"\n{train_data.classes = }")
 
+# Visualizing sample from Dataset
+image, label = train_data[10]
+plt.imshow ( image.squeeze() ) # image shape is [1, 28, 28] (colour channels, height, width)
+plt.title ( train_data.classes[label] + " " + str ( label ) )
+plt.show ()
 ### Create DataLoader's for our training and test sets.
 
 # Setup the batch size hyperparameter
@@ -137,12 +166,19 @@ test_dataloader = DataLoader ( test_data ,
 )
 
 # Let's check out what we've created
-print(f"Dataloaders: {train_dataloader, test_dataloader}") 
-print(f"Length of train dataloader: {len(train_dataloader)} batches of {BATCH_SIZE}")
-print(f"Length of test dataloader: {len(test_dataloader)} batches of {BATCH_SIZE}")
+print(f"{train_dataloader =}, {test_dataloader =}") 
+print(f"Length of train dataloader: {len ( train_dataloader ) } batches of {BATCH_SIZE}")
+print(f"Length of test dataloader: {len ( test_dataloader ) } batches of {BATCH_SIZE}")
+
+# Visualizing sample from DataLoader
+image, label = train_data[10]
+plt.imshow ( image.squeeze() ) # image shape is [1, 28, 28] (colour channels, height, width)
+plt.title ( train_data.classes[label] + " " + str ( label ) )
+plt.show ()
 
 # Create an instance of the model and send it to target device
-model_3 = CircleModelV3 ().to ( 'cpu' )
+torch.manual_seed(42)
+model_2 = FashionMNISTModelV2 ( input_shape = 1 , hidden_units = 10 , output_shape = len ( train_data.classes ) )
 # print ( f"\n{model_3 = }" )
 # print ( f"\n{model_3.state_dict () = }")
 
